6d3c7c9f7d8309732cf2666612d082d112c83113
allwen <ArianGhasempour40@Gmail.com>
2016-06-12 22:47:20 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,10 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'supergroup' then -- Put everything you like :)
+  if matches[1]:lower() == 'sync' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /sync ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "supergroup" then -- Put everything you like :)
+    if matches[2] == "sync" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (supergroup)",
-	"^[#!/]([Ss]upergroup)"
+    "^[#!/]([Ss]tats) (sync)",
+	"^[#!/]([Ss]ync)"
     }, 
   run = run
 }
