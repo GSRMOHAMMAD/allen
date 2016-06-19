e2d7cf862c47af3086b9e59c40b6a1f0d5306636
allwen <ArianGhasempour40@Gmail.com>
2016-06-19 16:22:13 +0430

    Update optima.lua

--- plugins/optima.lua
+++ plugins/optima.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-   send_document(get_receiver(msg), "./data/sticker/animation.jpg", ok_cb, false)
+   send_document(get_receiver(msg), "./data/animation.jpg", ok_cb, false)
 end
 
 return {
